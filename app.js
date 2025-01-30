fc94a0e0b7e0ddecbdd8d7dc89366a23ef009010
Rosas <xebasox@gmail.com>
2025-01-30 13:18:09 -0500

    Updated code

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigoSecreto;
+let amigos = [];
+let numeroAmigos = 0;
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    limpiarCaja();
+    if (amigo != ''){
+        console.log(`Introduced ${amigo}`);
+        amigos.push(amigo);
+        actualizarAmigos();
+        return;
+    }
+    else {
+        alert('Introduce un nombre válido');
+        return;
+    }
+}
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+    return;
+}
+function actualizarAmigos(){
+    numeroAmigos = amigos.length;
+    let listaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML = '';
+    for (var i = 0; i <= (amigos.length-1); i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
